8028516206e33da73321ea2ae9a59c9ea6cdc236
Thomas Wallsmith <thomaswallsmith@gmail.com>
2021-09-10 12:33:13 -0400

    Completed exercise06.

---
+++ exercise06/src/main/java/Solution/Solution06.java
@@ -0,0 +1,34 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Thomas Wallsmith
+ */
+
+package Solution;
+
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class Solution06 {
+    public static void main(String[] args) {
+
+        int currentAge;
+        int retireAge;
+        int currentYear;
+        int retireYear;
+        int meantime;
+
+        currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf("What is your current age? ");
+        currentAge = scanner.nextInt();
+        System.out.printf("At what age would you like to retire? ");
+        retireAge = scanner.nextInt();
+
+        meantime = retireAge-currentAge;
+        retireYear = currentYear+meantime;
+
+        System.out.printf("You have %d years left until you can retire.%n", meantime);
+        System.out.printf("It's %d, so you can retire in %d.%n", currentYear, retireYear);
+    }
+}
