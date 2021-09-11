396b0276ca9e85ba8a4a1a9ab613c48928c50afd
Thomas Wallsmith <thomaswallsmith@gmail.com>
2021-09-10 22:25:40 -0400

    Completed exercise11

---
+++ exercise11/src/main/java/Solution11/Solution11.java
@@ -0,0 +1,31 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Thomas Wallsmith
+ */
+
+/*
+    PSEUDOCODE:
+        Prompt & read for number of euros to be exchanged
+        Prompt & read for exchange rate
+        Calculate USD value
+        Print information to console
+ */
+
+package Solution11;
+
+import java.util.Scanner;
+
+public class Solution11 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("How many euros are you exchanging? ");
+        double euros = sc.nextDouble();
+        System.out.print("What is the exchange rate? ");
+        double rate = sc.nextDouble();
+
+        double result = rate*euros;
+        System.out.printf("%.2f euros at an exchange rate of %.2f is\n%.2f U.S. dollars.\n", euros, rate, result);
+    }
+}
